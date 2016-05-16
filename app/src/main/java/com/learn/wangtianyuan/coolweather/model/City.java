722c25a902a38fe24479fbb9100dbcf9c2ab80a5
windless <windless.d@gmail.com>
2016-05-16 22:44:14 +0800

    添加完数据库操作方法

---
+++ app/src/main/java/com/learn/wangtianyuan/coolweather/model/City.java
@@ -0,0 +1,45 @@
+package com.learn.wangtianyuan.coolweather.model;
+
+/**
+ * Created by wangtianyuan on 16/5/16.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
